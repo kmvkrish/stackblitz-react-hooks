73d893b91a46798542cba47213eb4d34d5cdea0d
kmvkrish <krishnamanojvaranasi.kmv@gmail.com>
2023-07-30 22:25:21 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,21 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './style.css';
 
 import { recipes as data } from './data/data.js';
 import RecipeItem from './Components/RecipeItem.js';
 
 export default function App({ roomId }) {
+  const [text, setText] = useState('');
+
+  const handleSubmit = (e) => alert('Submitting');
+
   return (
     <>
-      <h1>Recipes</h1>
-      {data.map((recipe, index) => (
-        <RecipeItem
-          key={index}
-          id={recipe.id}
-          name={recipe.name}
-          ingredients={recipe.ingredients}
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
         />
-      ))}
+        <input type="button" value="Submit" onClick={handleSubmit} />
+      </form>
     </>
   );
 }
